08111c2e91d45f0de1c493576847763de00df33d
tomjoy75 <tomjoy75@gmail.com>
2025-03-03 23:30:47 +0100

    Few modif on exo4

--- module00/ex04/main.c
+++ module00/ex04/main.c
@@ -15,41 +15,52 @@ int	is_pushed(char pin){
 	return (0);
 }
 
-int	is_lighted(char port){
-	if ((PINB & (1<<port)) == 1)
-	       return (1);
-	return (0);	
-}
+// int	is_lighted(char port){
+// 	if ((PINB & (1<<port)) == 1)
+// 	       return (1);
+// 	return (0);	
+// }
 
 void	actualise(char i){
-	PORTB &= ~(0b00010111);  // Effacer PB0 à PB3
-	PORTB |= (i & 0b00010111);  // Mettre à jour les LEDs
+	PORTB &= ~(0b00000111);  // Erase PB0 to PB2
+	PORTB &= ~(1 << PB4);	// Erase PB4
+
+	PORTB |= (i & 0b00000111);  // Actualise LEDs
+	if (i & 0b00001000){ // If the fourth bit is 1
+		PORTB |= 0b00010000; // Light D4
+//		PORTB &= 0b00010111; // Erase fourth bit
+	}
 }
 
 int	main(void){
 
 	char sw1_flag = 0;
 	char sw2_flag = 0;
-	DDRB |= (1<<PB0);
-	DDRB |= (1<<PB1);
-	DDRB |= (1<<PB2);
-	DDRB |= (1<<PB4);
+	// DDRB |= (1<<PB0);
+	// DDRB |= (1<<PB1);
+	// DDRB |= (1<<PB2);
+	// DDRB |= (1<<PB4);
+	DDRB |= 0b00010111;
 	DDRD &= ~(1<<PD2);
 	DDRD &= ~(1<<PD4);
 	char	i = 0;
 	while (1){
 		while (is_pushed(PD2)){
 			if (!sw1_flag){
+				if (i < 16)
+					i++;
 				// Actualiser les lights sur ++i
-				actualise(++i);
+				actualise(i);
 				sw1_flag = 1;
 			}
 		}
 		
 		while (is_pushed(PD4)){
 			if (!sw2_flag){
+				if (i > 0)
+					i--;
 				// Actualiser les lights sur --i
-				actualise(--i);
+				actualise(i);
 				sw2_flag = 1;
 			}
 		}
