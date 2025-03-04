e321947f533343a21d074eb8a30726ecffa50fd4
Thomas Joyeux <tjoyeux@made-f0Dr3s3.clusters.42paris.fr>
2025-03-04 17:24:34 +0100

    Several modifications on ex00
    - Added a .h
    - Change on gestion of the clock using TCCR1B
    - Usage of prescaling

--- module01/ex00/main.c
+++ module01/ex00/main.c
@@ -1,14 +1,16 @@
-#include <avr/io.h>
-#include <util/delay.h>
-
-#define DELAY 500
+#include "main.h"
 
 int	main(void){
 
 	DDRB |= (1<<PB1); // activate data register for Port B on PB1
+	TCCR1B |= (1<<CS12); // set prescaler to 256
+	TCNT1 = 0; // set timer to 0
+	uint32_t target_timer = NEW_FREQ / 2 - 1;
 	while (1){
-		PORTB ^= (1<<PB1); // XOR Operation to invert the output
-		_delay_ms(DELAY); 
+		if (TCNT1 >= target_timer){
+			PORTB ^= (1<<PB1); // XOR Operation to invert the output
+			TCNT1 = 0;
+		}
 	}
 	return (0);
 }
