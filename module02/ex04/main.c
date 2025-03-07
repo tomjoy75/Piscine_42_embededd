cc65de58bd4fc380e069fb10df3afa72f8d148cd
Thomas Joyeux <tjoyeux@made-f0Cr2s1.clusters.42paris.fr>
2025-03-07 18:49:26 +0100

    ex04 work in progress

---
+++ module02/ex04/main.c
@@ -0,0 +1,70 @@
+#include "main.h"
+
+volatile char	c;
+char	uart_buffer[64];
+int		uart_index = 0;
+
+
+void	uart_init(void){
+	int	baud_prescale = (uint16_t)(( F_CPU /16./BAUD) - 0.5);
+	//UCSR0A |= (1 << U2X0);
+	UBRR0H = (baud_prescale >> 8);
+	UBRR0L = baud_prescale;
+	UCSR0B = (1 << TXEN0) | (1 << RXEN0) /*| (1 << RXCIE0)*/;  // Activate Tx and Rx and interrupt when a char is received
+	UCSR0C = (1 << UCSZ01) | (1 << UCSZ00); // 8 bits mode
+    //UCSR0B |= (1 << UDRIE0) | (1 << RXCIE0);  // Active l’interruption d’envoi UART et quand un caractere est recu
+	sei();
+}
+
+void	uart_tx(void){
+	while (!(UCSR0A & (1 << UDRE0)));
+	UDR0 = c;
+}
+
+// ISR(USART_RX_vect){ // Interruption declenched by an input char
+
+// 	c = UDR0;
+// 	if (c >= 'a' && c <= 'z')
+// 		c -= 32;
+// 	else if (c >= 'A' && c <= 'Z')
+// 		c += 32;
+// 	uart_tx();
+// }
+
+void uart_printstr(const char *str) {
+	int i = 0;
+	while (str[i] && i < sizeof(uart_buffer) - 1){
+		uart_buffer[i] = str[i];
+		i++;
+	}
+	uart_buffer[i] = '\0';
+    uart_index = 0;
+    UCSR0B |= (1 << UDRIE0);  // Active l’interruption d’envoi UART
+}
+
+
+void	prompt(void){
+	uart_printstr("Enter your login:\r\n");
+	uart_printstr("\tusername:\r\n");
+}
+
+// ISR UART → Envoie les caractères un par un
+ISR(USART_UDRE_vect) {
+	//	PORTB ^= (1<< PB1);
+	if (uart_buffer[uart_index]) {
+		UDR0 = uart_buffer[uart_index++];
+	} else {
+		UCSR0B &= ~(1 << UDRIE0);  // Désactive l'interruption une fois terminé
+	}
+}
+
+int	main(void){
+	uart_init();
+	prompt();	
+	while(1){
+	}
+	return (0);
+}
+
+// 1-Send back char with interrupts
+// 2-Manage /uppercase
